c82c35fd484730f35c75737c1de3a80a347a5101
Koichi5 <koichi20021217@gmail.com>
2025-06-30 00:21:01 +0900

    fix: Appbarのタイトル変更

--- lib/weather/widgets/weather_view.dart
+++ lib/weather/widgets/weather_view.dart
@@ -10,7 +10,7 @@ class WeatherView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Tokyo Weather Edited')),
+      appBar: AppBar(title: const Text('Tokyo Weather')),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
